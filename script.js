0d43046c042cf5297485f3abccec8f7719b74538
Jose <jose.mar.123@hotmail.com>
2023-01-26 20:37:18 -0600

    Add aditonal style
    Changed default image, made images rounder, added bootstrap containers.

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const lastNameColumn = document.getElementsByClassName("last-name");
 const emailColumn = document.getElementsByClassName("email");
 const avatarColumn = document.getElementsByClassName("avatar");
 
-const myData = fetch("https://reqres.in/api/users?delay=1").then((res) =>
-  res.json().then((data) => {
-    const userData = data.data;
+const populateTable = () =>
+  fetch("https://reqres.in/api/users?delay=1").then((res) =>
+    res.json().then((data) => {
+      const userData = data.data;
 
-    for (let i = 0; i < userData.length; i++) {
-      nameColumn[i].innerHTML = userData[i].first_name;
-      lastNameColumn[i].innerHTML = userData[i].last_name;
-      emailColumn[i].innerHTML = userData[i].email;
-      avatarColumn[i].src = userData[i].avatar;
-    }
-  })
-);
-
-console.log(typeof myData);
+      for (let i = 0; i < userData.length; i++) {
+        nameColumn[i].innerHTML = userData[i].first_name;
+        lastNameColumn[i].innerHTML = userData[i].last_name;
+        emailColumn[i].innerHTML = userData[i].email;
+        avatarColumn[i].src = userData[i].avatar;
+      }
+    })
+  );
