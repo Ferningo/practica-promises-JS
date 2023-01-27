93458054a0165ad9ba1a4b27706bd782d0953154
Jose <jose.mar.123@hotmail.com>
2023-01-26 18:33:05 -0600

    Add avatar images to table
    Added code to include avatar pictures in table rows and added
    bootstrap initial setup.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nameColumn = document.getElementsByClassName("name");
 const lastNameColumn = document.getElementsByClassName("last-name");
 const emailColumn = document.getElementsByClassName("email");
+const avatarColumn = document.getElementsByClassName("avatar");
 
 const myData = fetch("https://reqres.in/api/users?delay=1").then((res) =>
   res.json().then((data) => {
@@ -10,6 +11,9 @@ const myData = fetch("https://reqres.in/api/users?delay=1").then((res) =>
       nameColumn[i].innerHTML = userData[i].first_name;
       lastNameColumn[i].innerHTML = userData[i].last_name;
       emailColumn[i].innerHTML = userData[i].email;
+      avatarColumn[i].src = userData[i].avatar;
     }
   })
 );
+
+console.log(typeof myData);
